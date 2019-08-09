a54a42174dac132202465c60b4d738b8241d4609
Harrison McCullough <harrison.mccullough@romariccorp.com>
2019-08-09 16:22:14 -0600

    Add dottilde to delete emacs temporary files

--- source/50_file.sh
+++ source/50_file.sh
@@ -41,6 +41,7 @@ alias df="df -h"
 
 # Recursively delete `.DS_Store` files
 alias dsstore="find . -name '*.DS_Store' -type f -ls -delete"
+alias dottilde="find . -name '*~' -type f -ls -delete"
 
 # Aliasing eachdir like this allows you to use aliases/functions as commands.
 alias eachdir=". eachdir"
