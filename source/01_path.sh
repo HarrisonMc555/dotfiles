bed9cc8b41723c757a0840a0ce8d0688f4eaab41
Harrison McCullough <mccullough.harrison@gmail.com>
2019-08-14 16:03:51 -0600

    Rearrange paths

--- source/01_path.sh
+++ source/01_path.sh
@@ -1,12 +1,12 @@
 paths=(
+  /opt/gradle/gradle-5.5.1/bin
+  ~/.cargo/bin
+  /usr/local/sbin
+  /usr/local/bin
   ~/bin
   ~/.local/bin
   ~/.local/sbin
   "$DOTFILES"/bin
-  /usr/local/sbin
-  /usr/local/bin
-  "$HOME"/.cargo/bin
-  /opt/gradle/gradle-5.5.1/bin
 )
 
 export PATH
