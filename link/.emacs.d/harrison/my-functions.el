0989b5e1106daf548e162f251c0f4aef978814ca
Harrison McCullough <mccullough.harrison@gmail.com>
2019-09-27 09:29:24 -0600

    Add key to open all marked buffers in Emacs dired mode

--- link/.emacs.d/harrison/my-functions.el
+++ link/.emacs.d/harrison/my-functions.el
@@ -249,5 +249,14 @@ The same result can also be be achieved by \\[universal-argument] \\[unhighlight
   (delete-char 1)
   (delete-horizontal-space))
 
+(eval-after-load "dired"
+  '(progn
+     (define-key dired-mode-map "F" 'my-dired-find-file)
+     (defun my-dired-find-file (&optional arg)
+       "Open each of the marked files, or the file under the point, or when prefix arg, the next N files "
+       (interactive "P")
+       (let* ((fn-list (dired-get-marked-files nil arg)))
+         (mapc 'find-file fn-list)))))
+
 (provide 'my-functions)
 ;;; my-functions.el ends here
