65bf5fb46a88260dea5b46b8572c94a56bd25011
Harrison McCullough <mccullough.harrison@gmail.com>
2019-09-30 12:07:22 -0600

    Improve svn revision function to cache results of svn log
    This allows the preview window to load much more quickly, since it doesn't have
    to make a network call to load the commit message of every revision.

--- source/70_svn_fzf.sh
+++ source/70_svn_fzf.sh
@@ -22,7 +22,7 @@ if is_available svn && is_available fzf; then
         is_in_svn_repo || return
         svn status |
             filter_out_directories |
-            fzf-down -m --ansi --nth -1 \
+            fzf-down -m --nth -1 \
                      --preview '(svn diff -- {-1} | colordiff | sed 1,5d; bat {-1} 2> /dev/null || cat {-1}) | head -500' |
             cut -c9-
     }
@@ -55,11 +55,17 @@ Ctrl-H: Full URL'
     # Fuzzy searches for a revision(s)
     function sr() {
         is_in_svn_repo || return
-        svn_short_log --incremental |
-            fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
+        log_file="$(mktemp)"
+        svn log --incremental |
+            tee "$log_file" |
+            _svn_short_log |
+            fzf-down --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
                      --header 'Press CTRL-S to toggle sort' \
-                     --preview 'sed -E "s/^r([0-9]+) .*/\1/" <<< {} | xargs svn log -v -r | head -'$LINES |
+                     --preview "svn_find_revision -v rev=\$(grep -oE ^r[0-9]+ <<< {}) ${log_file} | head -"$LINES \
+                     --preview-window=down:50% \
+                     --height 70% |
             sed -E 's/^r([0-9]+) .*/\1/'
+        rm "$log_file"
     }
 
     bind '"\er": redraw-current-line'
