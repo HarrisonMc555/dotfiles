de5a02feeb00873bf35040c850f186b03f205b64
Harrison McCullough <mccullough.harrison@gmail.com>
2019-10-04 14:46:22 -0600

    Add application aliases for macOS

---
+++ source/50_applications.sh
@@ -0,0 +1,7 @@
+if is_osx; then
+    alias safari='open -a safari'
+    alias chrome="open -a google\ chrome"
+    alias f='open -a Finder '
+    alias fh='open -a Finder .'
+    alias lmk="say 'Process complete.'"
+fi
