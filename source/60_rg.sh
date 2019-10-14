8354a30e342876bf8d509e76797e821564c066c3
Harrison McCullough <mccullough.harrison@gmail.com>
2019-10-14 13:01:31 -0600

    Add --heading to ripgrep functions

--- source/60_rg.sh
+++ source/60_rg.sh
@@ -3,15 +3,15 @@ if is_available rg; then
     export RIPGREP_CONFIG_PATH=~/.ripgreprc
 
     function rgl() {
-        rg -n --color=always --sort path "$@" | less
+        rg -n --color=always --sort path --heading "$@" | less
     }
 
     function rgh() {
-        rg -n --hidden --no-ignore "$@"
+        rg -n --hidden --no-ignore --heading "$@"
     }
 
     function rghl() {
-        rg -n --hidden --no-ignore --color=always --sort path  "$@" | less
+        rg -n --hidden --no-ignore --color=always --sort path --heading "$@" | less
     }
 
     function rglh() {
