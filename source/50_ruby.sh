7293300e1b4aa60af5ca5b2fd2be964f5f93309b
Harrison McCullough <mccullough.harrison@gmail.com>
2019-11-14 09:52:30 -0700

    Source rbenv

--- source/50_ruby.sh
+++ source/50_ruby.sh
@@ -1,6 +1,8 @@
-export PATH
-
 if [[ -e /etc/profile.d/rvm.sh ]]; then
   # rvm init
   source /etc/profile.d/rvm.sh
+fi
+
+if [[ -d "$HOME/.rbenv/" ]]; then
+    export PATH="$HOME/.rbenv/shims:$PATH"
 fi
