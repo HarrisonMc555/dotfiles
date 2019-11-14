1147cc2f65047e313ea43f2f59a559ec680242f6
Harrison McCullough <mccullough.harrison@gmail.com>
2019-11-14 09:22:34 -0700

    Fix bug in alias 'f'
    This alias is for the application "Finder" in Mac OS. It used to have a space at
    the end of the alias. For some reason, that caused the following argument to get
    expanded as an alias. Specifically, "f .." was expanded to "open -a Finder  cd
    ..", since ".." is an alias for "cd ..". This was very strange.

--- source/50_applications.sh
+++ source/50_applications.sh
@@ -2,7 +2,7 @@ if is_osx; then
     alias safari='open -a safari'
     alias chrome="open -a google\ chrome"
     alias firefox='open -a firefox'
-    alias f='open -a Finder '
+    alias f='open -a Finder'
     alias fh='open -a Finder .'
     alias lmk="say 'Process complete.'"
 fi
