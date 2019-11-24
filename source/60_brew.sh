83ae315ac10b27489aed89ba0b0acc2f8d2f904e
Harrison McCullough <mccullough.harrison@gmail.com>
2019-11-24 16:38:36 -0700

    Disable homebrew command not found
    This adds several seconds to sessino startup, which is too long for me.

--- source/60_brew.sh
+++ source/60_brew.sh
@@ -1,5 +1,6 @@
-if is_osx && is_available brew; then
-    if brew command command-not-found-init > /dev/null 2>&1; then
-        eval "$(brew command-not-found-init)"
-    fi
-fi
+# Do not use this, it is too slow
+# if is_osx && is_available brew; then
+#     if brew command command-not-found-init > /dev/null 2>&1; then
+#         eval "$(brew command-not-found-init)"
+#     fi
+# fi
