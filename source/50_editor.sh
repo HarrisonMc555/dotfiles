45c0274f0e21e999008e16ac413853f288dd2a4b
Harrison McCullough <mccullough.harrison@gmail.com>
2020-01-30 09:31:34 -0700

    Add is_emacs_deamon_running Bash function

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -7,3 +7,18 @@ export VISUAL='emacsclient -a ""'
 alias emacs="emacsclient -n -a ''"
 alias emacsw="emacsclient -c -n -a ''"
 alias emacst="emacsclient -c -t -a ''"
+
+if is_available rg; then
+    function is_emacs_deamon_running() {
+        ps aux |
+            rg '/Applications/Emacs Daemon.app/Contents/MacOS/Emacs Daemon$' \
+                 -q
+    }
+else
+    function is_emacs_deamon_running() {
+        ps aux |
+            grep '/Applications/Emacs Daemon.app/Contents/MacOS/Emacs Daemon$' \
+                 -q
+    }
+fi
+export -f is_emacs_deamon_running
