1229476a9f29e8bef842c02fd35b43856876aad4
Harrison McCullough <mccullough.harrison@gmail.com>
2020-03-22 18:23:56 -0600

    Changes to scripts for Ubuntu compatibility

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -11,16 +11,20 @@ alias emacs="emacsclient -n -a ''"
 alias emacsw="emacsclient -c -n -a ''"
 alias emacst="emacsclient -c -t -a ''"
 
-if is_osx; then
-    function is_emacs_deamon_running() {
-        local name='^/Applications/Emacs.app/Contents/MacOS/Emacs-x86_64-10_14 --bg-daemon'
-        ps_process_names | _grep "$name" -q
-    }
+function is_emacs_deamon_running() {
+    local name
+    if is_osx; then
+        name='^/Applications/Emacs.app/Contents/MacOS/Emacs-x86_64-10_14 --bg-daemon'
+    else
+        name='^/usr/bin/emacs --daemon'
+    fi
+    ps_process_names | _grep "$name" -q
+}
+export -f is_emacs_deamon_running
 
+if is_osx; then
     function is_emacs_gui_running() {
         ps_process_names | _grep '^/Applications/Emacs.app/' -q
     }
-
-    export -f is_emacs_deamon_running
     export -f is_emacs_gui_running
 fi
