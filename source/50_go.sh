9f321fee6cc2577603c31f037b0c02c4977d366d
Harrison McCullough <mccullough.harrison@gmail.com>
2020-04-16 14:52:10 -0600

    Update Go to not export GOROOT

--- source/50_go.sh
+++ source/50_go.sh
@@ -3,15 +3,7 @@
 if is_available go; then
 
     export GOPATH="${HOME}/.go"
-    if is_osx; then
-        # brew --prefix is kind of slow
-        brew_prefix="$(dirname "$(dirname "$(type -p brew)")")"
-        export GOROOT="$brew_prefix"/opt/go
-        # export GOROOT="$(brew --prefix golang)/libexec"
-    else
-        export GOROOT="/usr/local/go"
-    fi
-    export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+    export PATH="$PATH:${GOPATH}/bin"
     test -d "${GOPATH}" || mkdir "${GOPATH}"
     test -d "${GOPATH}/src/github.com" || mkdir -p "${GOPATH}/src/github.com"
 
