18cc49cd3f0eb2a1407f5fd6d4fecb7129df95a8
Harrison McCullough <mccullough.harrison@gmail.com>
2020-06-21 16:50:57 -0600

    Add mysql and mycli configuration

---
+++ source/60_mysql.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+if is_available mycli; then
+    function mycli() {
+        args=()
+        if [[ "$DB_PORT" ]]; then
+            args+=("--port=$DB_PORT")
+        fi
+        if [[ "$DB_USER" ]]; then
+            args+=("--user=$DB_USER")
+        fi
+        get_db_passwd
+        if [[ "$DB_PASS" ]]; then
+            args+=("--password=$DB_PASS")
+        fi
+        # Add this here instead of ~/.my.cnf because mysqladmin chokes on it
+        args+=("--defaults-file=~/.my.cnf.client")
+        "$(which mycli)" "${args[@]}" "$@"
+    }
+fi
