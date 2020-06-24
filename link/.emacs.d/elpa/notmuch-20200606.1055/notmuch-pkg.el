2eb53b7ea8412b5b39cd0add3e7f938a56ea82d4
Harrison McCullough <mccullough.harrison@gmail.com>
2020-06-24 15:17:01 -0600

    Update notmuch

---
+++ link/.emacs.d/elpa/notmuch-20200606.1055/notmuch-pkg.el
@@ -0,0 +1,4 @@
+(define-package "notmuch" "20200606.1055" "run notmuch within emacs" 'nil :commit "81057164cddf6a5c1d4c30a23767c4de8e615c1c" :url "https://notmuchmail.org/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
