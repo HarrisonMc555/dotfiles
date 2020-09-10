22fc5ca0833fa9a81d302cc00bea1692a26f32b3
Harrison McCullough <mccullough.harrison@gmail.com>
2020-09-10 16:03:59 -0600

    Print message when not in git repo for git fzf functions

--- source/70_git_fzf.sh
+++ source/70_git_fzf.sh
@@ -3,6 +3,13 @@
 
 if is_available git && is_available fzf; then
 
+    function __check_git_repo() {
+        if ! is_in_git_repo; then
+            >&2 echo "Not in Git repository. Exiting."
+            return 1
+        fi
+    }
+
     function is_in_git_repo() {
         git rev-parse HEAD > /dev/null 2>&1
     }
@@ -14,8 +21,16 @@ if is_available git && is_available fzf; then
     # Git file
     # Fuzzy searches for modified or untracked file(s)
     function gf() {
-        is_in_git_repo || return
-        git -c color.status=always status --short |
+        __check_git_repo || return 1
+        if [[ $# -gt 1 ]]; then
+            >&2 echo "Usage: gf [PATH]"
+            return 1
+        fi
+        if [[ "$1" ]] && [[ ! -e "$1" ]]; then
+            >&2 echo "Argument must be a valid path"
+            return 1
+        fi
+        git -c color.status=always status --short "$@" |
             fzf-down -m --ansi --nth 2..,.. \
                      --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
             cut -c4- | sed 's/.* -> //'
@@ -24,7 +39,11 @@ if is_available git && is_available fzf; then
     # Git branch
     # Fuzzy searches for branch(es)
     function gb() {
-        is_in_git_repo || return
+        __check_git_repo || return 1
+        if [[ $# -ne 0 ]]; then
+            >&2 echo "gt does not accept any parameters"
+            return 1
+        fi
         local out key branch
         out=("$(git branch -a --color=always | grep -v '/HEAD\s' | sort |
             fzf-down --ansi --multi --tac --preview-window right:70% \
@@ -41,7 +60,11 @@ if is_available git && is_available fzf; then
     # Git tag
     # Fuzzy searches for tag(s)
     function gt() {
-        is_in_git_repo || return
+        __check_git_repo || return 1
+        if [[ $# -ne 0 ]]; then
+            >&2 echo "gt does not accept any parameters"
+            return 1
+        fi
         git tag --sort -version:refname |
             fzf-down --multi --preview-window right:70% \
                      --preview 'git show --color=always {} | head -'"$LINES"
@@ -50,7 +73,7 @@ if is_available git && is_available fzf; then
     # Git history (SHA)
     # Fuzzy searches for a commit SHA(s)
     function gh() {
-        is_in_git_repo || return
+        __check_git_repo || return 1
         git log --date=short --format="%C(green)%C(bold)%cd %C(auto)%h%d %s (%an)" --graph --color=always |
             fzf-down --ansi --no-sort --reverse --multi --bind 'ctrl-s:toggle-sort' \
                      --header 'Press CTRL-S to toggle sort' \
@@ -61,7 +84,11 @@ if is_available git && is_available fzf; then
     # Git remote
     # Fuzzy searches for a git remote
     function gr() {
-        is_in_git_repo || return
+        __check_git_repo || return 1
+        if [[ $# -ne 0 ]]; then
+            >&2 echo "gr does not accept any parameters"
+            return 1
+        fi
         git remote -v | awk '{print $1 "\t" $2}' | uniq |
             fzf-down --tac \
                      --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" {1} | head -200' |
