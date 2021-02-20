21c3570b2959ef58c328fd6f2e94cc5427044235
Harrison McCullough <mccullough.harrison@gmail.com>
2021-02-19 17:19:11 -0700

    Improve 'gf' git function/keyboard shortcut

--- source/70_git_fzf.sh
+++ source/70_git_fzf.sh
@@ -33,7 +33,9 @@ if is_available git && is_available fzf; then
         git -c color.status=always status --short "$@" |
             fzf-down -m --ansi --nth 2..,.. \
                      --preview '(git diff --color=always -- {-1} | sed 1,4d; cat {-1}) | head -500' |
-            cut -c4- | sed 's/.* -> //'
+            cut -c4- |
+            sed 's/.* -> //' |
+            cat
     }
 
     # Git branch
