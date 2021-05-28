fcdb77b592a98ea13ef25c135a927ddbf380257b
Harrison McCullough <mccullough.harrison@gmail.com>
2021-05-28 16:00:39 -0600

    Improve f and fh aliases/functions
    The `f` and `fh` aliases are used to open files and directories in the
    native macOS Finder application.
    
    However, if you used `f` on a file, it would simply open the file with
    the default application, much like the `open` command (as if you
    double-clicked the file in the Finder application itself). If I want
    to do that, I will use the `open` command. When I use the `f` command,
    I want it to open in Finder. To support that, you can use `open -R`,
    which "reveals" the file in Finder.
    
    The new `f` function will use `open -R` for files and `open -a Finder
    DIR` for directories. The `fh` function expects no arguments and opens
    the current directory in Finder.

--- source/50_applications.sh
+++ source/50_applications.sh
@@ -4,8 +4,22 @@ if is_osx; then
     alias safari='open -a safari'
     alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
     alias firefox='open -a firefox'
-    alias f='open -a Finder'
-    alias fh='open -a Finder .'
+    f() {
+        for arg in "$@"; do
+            if [[ -f "$arg" ]]; then
+                open -R "$arg"
+            else
+                open -a Finder "$arg"
+            fi
+        done
+    }
+    fh() {
+        if [[ $# -ne 0 ]]; then
+            >&2 echo "Usage: fh"
+            return 1
+        fi
+        open -a Finder .
+    }
     alias lmk="say '[[volm 0.15]] Process complete.'"
     alias say="say '[[volm 0.15]]'"
 
