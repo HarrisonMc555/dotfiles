3c07bc362321a0d5e2bf35f8268fd02eaa0f8888
Harrison McCullough <mccullough.harrison@gmail.com>
2022-06-07 10:53:24 -0700

    Move OSX file earlier in source sequence

--- source/20_osx.sh
+++ source/20_osx.sh
@@ -5,14 +5,19 @@ if is_osx; then
 
     # Add paths to beginning
     paths=(
-        /usr/local/bin
+        /opt/homebrew/bin
+        /opt/homebrew/sbin
+        /opt/homebrew/opt/coreutils/libexec/gnubin
         /usr/local/opt/coreutils/libexec/gnubin
+        /opt/homebrew/opt/mysql@5.7/bin
+        /usr/local/bin
+        /usr/bin
     )
 
     paths_rev=()
 
     for p in "${paths[@]}"; do
-        paths_rev=("$p" "${paths[@]}")
+        paths_rev=("$p" "${paths_rev[@]}")
     done
 
     for p in "${paths_rev[@]}"; do
@@ -25,4 +30,5 @@ if is_osx; then
     # Make 'less' more.
     [[ "$(type -P lesspipe.sh)" ]] && eval "$(lesspipe.sh)"
 
+    alias preview='open -a /System/Applications/Preview.app'
 fi
