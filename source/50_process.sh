3a78f15851c7575dca7d075de81275cdd971d5ef
Harrison McCullough <mccullough.harrison@gmail.com>
2022-06-07 10:50:28 -0700

    Do not use "procs" for psg function

--- source/50_process.sh
+++ source/50_process.sh
@@ -1,33 +1,16 @@
 #!/usr/bin/env bash
 
-if is_available procs; then
-    function psg() {
-        if [[ $# -lt 1 ]]; then
-            echo "Usage: psg PATTERN [, PATTERN*]"
-            return 1
-        fi
-        # `procs' appends extra spaces to the end of every line, I assume to
-        # make every line the same width. This is annoying when looking at less
-        # without `-S', so we use sed to strip them. The weird bit is for the
-        # color escape that `procs' also puts at the end of each line (for good
-        # reason).
-        procs --color=always "$@" |
-            sed -E 's/ +\[0m$/\[0m/' |
-            less -S
-    }
-else
-    function psg() {
-        if [[ $# -ne 1 ]]; then
-            echo "Usage: psg PATTERN"
-            return 1
-        fi
-        ps aux |
-            _grep -v -e "$_SEARCH_BINARY $*" -e "$_SEARCH_BINARY -v -e \"'$_SEARCH_BINARY $*'\"" |
-            _grep "$@" --color=always |
-            sed -e 'G' |
-            less
-    }
-fi
+function psg() {
+    if [[ $# -ne 1 ]]; then
+        echo "Usage: psg PATTERN"
+        return 1
+    fi
+    ps aux |
+        _grep -v -e "$_SEARCH_BINARY $*" -e "$_SEARCH_BINARY -v -e \"'$_SEARCH_BINARY $*'\"" |
+        _grep "$@" --color=always |
+        sed -e 'G' |
+        less
+}
 
 function ps_process_names() {
     if [[ $# -ne 0 ]]; then
