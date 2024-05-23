70bd3dffeec0e5815dd09ba0498f0e13f1cafa08
Harrison McCullough <mccullough.harrison@gmail.com>
2024-05-23 11:23:23 -0700

    Cd to exercism directory after downloading

--- source/60_exercism.sh
+++ source/60_exercism.sh
@@ -2,3 +2,14 @@
 
 [ -f ~/.config/exercism/exercism_completion.bash ] &&
     source ~/.config/exercism/exercism_completion.bash
+
+if is_available exercism; then
+    exercism () {
+        local out
+        readarray -t out < <(command exercism "$@")
+        printf '%s\n' "${out[@]}"
+        if [[ $1 == "download" && -d "${out[-1]}" ]]; then
+            cd "${out[-1]}" || return 1
+        fi
+    }
+fi
