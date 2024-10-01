d01fb68acff149c7c631fea78a38e0c9507ebb7f
Harrison McCullough <mccullough.harrison@gmail.com>
2024-09-30 21:47:48 -0700

    Install Emacs graphql packages

---
+++ link/.emacs.d/elpa/graphql-0.1.1/graphql-pkg.el
@@ -0,0 +1,14 @@
+(define-package "graphql" "0.1.1" "GraphQL utilities"
+  '((emacs "25"))
+  :commit "672dd9ebd7e67d8089388b0c484cd650e76565f3" :authors
+  '(("Sean Allred" . "code@seanallred.com"))
+  :maintainers
+  '(("Sean Allred" . "code@seanallred.com"))
+  :maintainer
+  '("Sean Allred" . "code@seanallred.com")
+  :keywords
+  '("hypermedia" "tools" "lisp")
+  :url "https://github.com/vermiculus/graphql.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
