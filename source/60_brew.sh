8401ecd0546af74b5e4414c4fb7c4aa870e0d2d9
Harrison McCullough <mccullough.harrison@gmail.com>
2025-02-08 22:01:46 -0700

    Check if Homebrew is installed before configuring

--- source/60_brew.sh
+++ source/60_brew.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-eval "$(brew shellenv)"
-if [[ $(uname -m) = arm* ]]; then
-    alias oldbrew=/usr/local/bin/brew
-fi
+if is_available brew; then
+    eval "$(brew shellenv)"
+    if [[ $(uname -m) = arm* ]]; then
+        alias oldbrew=/usr/local/bin/brew
+    fi
 
-# Do not use this, it is too slow
-# if is_osx && is_available brew; then
-#     if brew command command-not-found-init > /dev/null 2>&1; then
-#         eval "$(brew command-not-found-init)"
-#     fi
-# fi
+    # Do not use this, it is too slow
+    # if is_osx && is_available brew; then
+    #     if brew command command-not-found-init > /dev/null 2>&1; then
+    #         eval "$(brew command-not-found-init)"
+    #     fi
+    # fi
+fi
